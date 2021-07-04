9a12d5b5331c3f9a88b13b0799afbe7122bf48dc
Brandon Rodriguez <brancowcanada@gmail.com>
2021-07-03 20:06:13 -0500

    fix css

--- src/layouts/Home.jsx
+++ src/layouts/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 import AssignmentIcon from '@material-ui/icons/Assignment';import { metadata } from "../../metaDataPages";
 
 export const HomeHeader = () => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
      removeDeprecatedGapUtilities: true,
      purgeLayersByDefault: true,
    },
-   purge: false,
+   purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
    theme: {
      extend: {},
    },
